6dee8d58aaa747fe11271fb0060bb6bfc6597f0a
Narthana Epa <narthana.epa@gmail.com>
2023-02-07 23:46:55 +1100

    Attempt build

--- .buildkite/steps/build.sh
+++ .buildkite/steps/build.sh
@@ -2,8 +2,4 @@
 
 set -euf
 
-builder=$(docker buildx create --driver kubernetes --use)
-# shellcheck disable=SC2064 # we want the current $builder_name to be trapped, not the runtime one
-trap "docker buildx rm $builder || true" EXIT
-
-docker buildx build --progress=plain --builder "$builder" .
+buildctl --addr kube-pod://buildkitd-0 build --frontend dockerfile.v0 --local context=. --local dockerfile=./Dockerfile
