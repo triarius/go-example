18d9b5e95639b7f58db134c4862b5bc15bcb8282
Narthana Epa <narthana.epa@gmail.com>
2023-02-08 14:15:51 +1100

    Image

--- .buildkite/steps/build.sh
+++ .buildkite/steps/build.sh
@@ -2,7 +2,8 @@
 
 set -euf
 
-docker buildx create --name remote-buildkit --driver remote --driver-opt cacert=/buildkit/certs/ca.pem,cert=/buildkit/certs/cert.pem,key=/buildkit/certs/key.pem tcp://buildkitd.default.svc:1234 --use
-trap "docker buildx rm remote0buildkit" EXIT
+docker buildx create --name remote --driver remote --driver-opt cacert=/buildkit/certs/ca.pem,cert=/buildkit/certs/cert.pem,key=/buildkit/certs/key.pem tcp://buildkitd.default.svc:1234
+
+docker buildx use remote
 
 docker buildx build --tag="go-example:${BUILDKITE_COMMIT}" --platform=linux/amd64
