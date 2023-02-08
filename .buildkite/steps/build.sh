6a83a2ed97cbb2fee521a43251b27de6e737aa0c
Narthana Epa <narthana.epa@gmail.com>
2023-02-08 15:18:53 +1100

    Fix svc address

--- .buildkite/steps/build.sh
+++ .buildkite/steps/build.sh
@@ -2,6 +2,6 @@
 
 set -euf
 
-docker buildx create --name remote --driver remote --driver-opt cacert=/buildkit/certs/ca.pem,cert=/buildkit/certs/cert.pem,key=/buildkit/certs/key.pem tcp://buildkitd.default.svc:1234
+docker buildx create --name remote --driver remote --driver-opt cacert=/buildkit/certs/ca.pem,cert=/buildkit/certs/cert.pem,key=/buildkit/certs/key.pem tcp://buildkitd.buildkite.svc:1234
 docker buildx use remote
 docker buildx build --progress=plain --tag="go-example:${BUILDKITE_COMMIT}" --platform=linux/amd64 .
