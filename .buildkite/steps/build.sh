1c6b3d5a9e6d8a2432ca492f0f09cac9e3e84ed3
Narthana Epa <narthana.epa@gmail.com>
2023-02-08 14:19:18 +1100

    Plain progress

--- .buildkite/steps/build.sh
+++ .buildkite/steps/build.sh
@@ -3,7 +3,5 @@
 set -euf
 
 docker buildx create --name remote --driver remote --driver-opt cacert=/buildkit/certs/ca.pem,cert=/buildkit/certs/cert.pem,key=/buildkit/certs/key.pem tcp://buildkitd.default.svc:1234
-
 docker buildx use remote
-
-docker buildx build --tag="go-example:${BUILDKITE_COMMIT}" --platform=linux/amd64 .
+docker buildx build --progress=plain --tag="go-example:${BUILDKITE_COMMIT}" --platform=linux/amd64 .
