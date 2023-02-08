084491535fac92c996984af973772b5bb21e84ba
Narthana Epa <narthana.epa@gmail.com>
2023-02-08 15:25:50 +1100

    Use immediately

--- .buildkite/steps/build.sh
+++ .buildkite/steps/build.sh
@@ -2,6 +2,10 @@
 
 set -euf
 
-docker buildx create --name remote --driver remote --driver-opt cacert=/buildkit/certs/ca.pem,cert=/buildkit/certs/cert.pem,key=/buildkit/certs/key.pem tcp://buildkitd.buildkite.svc:1234
-docker buildx use remote
+docker buildx create \
+  --name remote \
+  --driver remote \
+  --driver-opt cacert=/buildkit/certs/ca.pem,cert=/buildkit/certs/cert.pem,key=/buildkit/certs/key.pem \
+  tcp://buildkitd.buildkite.svc:1234 \
+  --use
 docker buildx build --progress=plain --tag="go-example:${BUILDKITE_COMMIT}" --platform=linux/amd64 .
