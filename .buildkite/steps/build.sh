23dadb1c0297b0dc22b9a19d456abb80a4c32da7
Narthana Epa <narthana.epa@gmail.com>
2023-02-08 14:05:02 +1100

    Tag var

--- .buildkite/steps/build.sh
+++ .buildkite/steps/build.sh
@@ -5,4 +5,4 @@ set -euf
 docker buildx create --name remote-buildkit --driver remote --driver-opt cacert=/buildkit/certs/ca.pem,cert=/buildkit/certs/cert.pem,key=/buildkit/certs/key.pem tcp://buildkitd.default.svc:1234 --use
 trap "docker buildx rm remote0buildkit" EXIT
 
-docker buildx build --tag="${DOCKER_REGISTRY}/go-example:${BUILDKITE_COMMIT}" --platform=linux/amd64
+docker buildx build --tag="go-example:${BUILDKITE_COMMIT}" --platform=linux/amd64
