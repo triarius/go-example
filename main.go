4ef091d063103967298e6ca717f44f1dcc3e3678
Narthana Epa <narthana.epa@gmail.com>
2023-03-19 22:23:47 +1100

    Fix test

--- main.go
+++ main.go
@@ -11,13 +11,13 @@ import (
 func main() {
 	logger := zerolog.New(os.Stderr).With().Timestamp().Logger()
 
-	if err := run(os.Stdin, os.Stdout, os.Stderr); err != nil {
+	if err := Run(os.Stdin, os.Stdout, os.Stderr); err != nil {
 		logger.Error().Err(err).Msg("runtime error")
 		os.Exit(1)
 	}
 }
 
-func run(in io.Reader, out, err io.Writer) error {
+func Run(in io.Reader, out, err io.Writer) error {
 	fmt.Fprintf(out, "Hello World!\n")
 	return nil
 }
