83e4f95036816b8b5dd96e4291bc3ee6b906cc7e
Narthana Epa <narthana.epa@gmail.com>
2023-03-19 22:21:33 +1100

    Add zerolog

--- main.go
+++ main.go
@@ -12,11 +12,12 @@ func main() {
 	logger := zerolog.New(os.Stderr).With().Timestamp().Logger()
 
 	if err := run(os.Stdin, os.Stdout, os.Stderr); err != nil {
-		logger.Error().AnErr(err).Msg("runtime error")
+		logger.Error().Err(err).Msg("runtime error")
 		os.Exit(1)
 	}
 }
 
 func run(in io.Reader, out, err io.Writer) error {
 	fmt.Fprintf(out, "Hello World!\n")
+	return nil
 }
