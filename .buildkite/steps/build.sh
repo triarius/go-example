ec07103699a9d39edbfc7ae414de89f073c3c5f7
Narthana Epa <narthana.epa@gmail.com>
2023-03-21 12:53:50 +1100

    Use arm64 for builds

--- .buildkite/steps/build.sh
+++ .buildkite/steps/build.sh
@@ -8,4 +8,4 @@ docker buildx create \
   --driver-opt cacert=/buildkit/certs/ca.pem,cert=/buildkit/certs/cert.pem,key=/buildkit/certs/key.pem \
   tcp://buildkitd.buildkite.svc:1234 \
   --use
-docker buildx build --progress=plain --tag="go-example:${BUILDKITE_COMMIT}" --platform=linux/amd64 .
+docker buildx build --progress=plain --tag="go-example:${BUILDKITE_COMMIT}" --platform=linux/arm64 .
