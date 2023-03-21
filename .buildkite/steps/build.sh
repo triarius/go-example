1b9ad962e68afac086d2e1477e2d85551c429541
Narthana Epa <narthana.epa@gmail.com>
2023-03-21 17:49:00 +1100

    Push to repo

--- .buildkite/steps/build.sh
+++ .buildkite/steps/build.sh
@@ -2,10 +2,21 @@
 
 set -euf
 
+repo=253213882263.dkr.ecr.ap-southeast-2.amazonaws.com
+
 docker buildx create \
   --name remote \
   --driver remote \
   --driver-opt cacert=/buildkit/certs/ca.pem,cert=/buildkit/certs/cert.pem,key=/buildkit/certs/key.pem \
   tcp://buildkitd.buildkite.svc:1234 \
   --use
-docker buildx build --progress=plain --tag="go-example:${BUILDKITE_COMMIT}" --platform=linux/arm64 .
+
+echo --- :ecr: Logging into ECR
+aws ecr get-login-password --region ap-southeast-2 |
+  docker login \
+    --username AWS \
+    --password-stdin \
+    "$repo"
+
+echo --- :docker: Building Image
+docker buildx build --progress=plain --tag="$repo/go-example:${BUILDKITE_COMMIT}" --push --platform=linux/arm64 .
