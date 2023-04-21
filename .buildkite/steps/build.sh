e32bed07259739f84179a919001e3400629a4f6e
Narthana Epa <narthana.epa@gmail.com>
2023-04-21 17:45:27 +1000

    Back to no svc

--- .buildkite/steps/build.sh
+++ .buildkite/steps/build.sh
@@ -12,7 +12,7 @@ docker buildx create \
   --name remote \
   --driver remote \
   --driver-opt cacert=/buildkit/certs/ca.pem,cert=/buildkit/certs/cert.pem,key=/buildkit/certs/key.pem \
-  tcp://buildkitd.buildkite.svc:1234 \
+  tcp://buildkitd:1234 \
   --use
 
 echo --- :ecr: Logging into ECR
